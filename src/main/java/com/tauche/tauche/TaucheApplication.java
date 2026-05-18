440fdaed5483342635a173da806084367a11a232
swissschoggi <fynn.gaechter@gmail.com>
2026-05-18 10:33:38 +0200

    added springboot shit

---
+++ src/main/java/com/tauche/tauche/TaucheApplication.java
@@ -0,0 +1,13 @@
+package com.tauche.tauche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaucheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaucheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tauche/tauche/TaucheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tauche.tauche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaucheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
